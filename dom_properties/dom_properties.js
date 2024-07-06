6eb1c218c14dc7466fe43afc356ae79869e2e26b
DEVANSHI JOSHI <90313536+devanshijoshi15@users.noreply.github.com>
2024-07-07 01:05:48 +0530

    Add files via upload

---
+++ dom_properties/dom_properties.js
@@ -0,0 +1,8 @@
+// let h2 = document.querySelector("h2")
+// console.dir(h2)
+// console.dir(h2.innerText)
+// h2.innerText = h2.innerText + " from Devanshi";
+// console.dir(h2.innerText)
+
+let divs = document.querySelectorAll(".box")
+console.log(divs)
